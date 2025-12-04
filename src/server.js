2de3fa21f24f97f90fa1becd4a8d7255532faa1c
Trinh Huy Nam <huynam@Trinhs-MacBook-Pro-2.local>
2025-12-04 14:02:25 +0700

    fix cors

--- src/server.js
+++ src/server.js
@@ -16,12 +16,19 @@ const httpServer = http.createServer(app)
 const io = new Server(httpServer, {
     cors: {
         origin: "*",
-        methods: ["GET", "POST"]
+        methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+        allowedHeaders: ["*"],
+        credentials: false
     }
 })
 
 app.set("trust proxy", true)
-app.use(cors())
+app.use(cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: ["*"],
+    credentials: false
+}))
 app.use(express.json())
 app.use(express.static("public"))
 app.use(urlencoded({extended: true}))
